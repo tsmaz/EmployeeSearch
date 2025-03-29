02dd952628e2d85a970a7ffbc5d7c639b41646ca
tsmaz <tsmazich@cpp.edu>
2025-03-28 23:11:22 -0700

    Added initial implementation of searchEmployeeBySalary

--- employeeone.c
+++ employeeone.c
@@ -30,3 +30,14 @@ PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int tableSize, char 
     } 
     return NULL; 
 }
+
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, double targetSalary){ 
+    const PtrToConstEmployee endPtr = ptr + tableSize; 
+    for(; ptr < endPtr; ptr++)
+    { 
+        if(ptr->salary == targetSalary) 
+            return (PtrToEmployee) ptr; 
+    } 
+     return NULL;   
+} 
+ 
