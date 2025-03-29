e3cbfa6152217b31eec24f15e7266578347d599a
tsmaz <tsmazich@cpp.edu>
2025-03-28 22:54:48 -0700

    Initial implementation of searchEmployeeByPhone

--- employeeone.c
+++ employeeone.c
@@ -20,3 +20,13 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *
     } 
     return NULL; 
 }
+
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int tableSize, char * targetPhone){ 
+    const PtrToConstEmployee endPtr = ptr + tableSize; 
+    for(; ptr < endPtr; ptr++) 
+    { 
+        if(strcmp(ptr->phone, targetPhone) == 0) 
+            return (PtrToEmployee) ptr; 
+    } 
+    return NULL; 
+}
